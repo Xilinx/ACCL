1198814e3a2cb6447569bdbb193d1c4c33fdc245
Tristan Laan <tristan.laan@gmail.com>
2022-03-10 14:53:17 +0000

    Add run scripts and cmake project for simulation

---
+++ test/emulation/run.py
@@ -0,0 +1,77 @@
+import os
+from pathlib import Path
+import subprocess
+import argparse
+import sys
+import signal
+
+from matplotlib import use
+
+cwd = Path(__file__).parent.resolve()
+executable = cwd / 'cclo_emu'
+makefile = cwd / 'Makefile'
+xsim_path_tail = 'xsim.dir/ccl_offload_behav/xsimk.so'
+
+
+def gen_makefile():
+    p = subprocess.run(['cmake', '.'], cwd=cwd)
+    if p.returncode != 0:
+        print("cmake failed!")
+        sys.exit(1)
+
+
+def build_executable():
+    p = subprocess.run(['make'], cwd=cwd)
+    if p.returncode != 0:
+        print("make failed!")
+        sys.exit(1)
+
+
+def run_emulator(ranks: int, start_port: int, use_udp: bool):
+    args = ['mpirun', '-np', str(ranks), '--tag-output', str(executable),
+            'udp' if use_udp else 'tcp', str(start_port)]
+    print(' '.join(args))
+    with subprocess.Popen(args, cwd=cwd, stderr=subprocess.DEVNULL) as p:
+        try:
+            p.wait()
+        except KeyboardInterrupt:
+            try:
+                print("Stopping emulator...")
+                p.send_signal(signal.SIGINT)
+            except KeyboardInterrupt:
+                try:
+                    print("Force stopping emulator...")
+                    p.kill()
+                except KeyboardInterrupt:
+                    signal.signal(signal.SIGINT, signal.SIG_IGN)
+                    print("Terminating emulator...")
+                    p.terminate()
+
+
+def main(ranks: int, start_port: int, use_udp: bool, build: bool):
+    if not build and not executable.exists():
+        print(f"Executable {executable} does not exists!")
+        sys.exit(1)
+    else:
+        if not makefile.exists():
+            print("Makefile doesn't exist, running cmake...")
+            gen_makefile()
+        print("Building executable...")
+        build_executable()
+
+    print("Starting emulator...")
+    run_emulator(ranks, start_port, use_udp)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run ACCL emulator')
+    parser.add_argument('-n', '--nranks', type=int, default=1,
+                        help='How many ranks to use for the emulator')
+    parser.add_argument('-s', '--start-port', type=int, default=5500,
+                        help='Start port of emulator')
+    parser.add_argument('-u', '--udp', action='store_true', default=False,
+                        help='Run emulator over UDP instead of TCP')
+    parser.add_argument('--no-build', action='store_true', default=False,
+                        help="Don't build latest executable")
+    args = parser.parse_args()
+    main(args.nranks, args.start_port, args.udp, not args.no_build)
