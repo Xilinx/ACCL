8ef31bf63fbbecfe98b7905f6ba007b15894489c
Thomas B. Preußer <thomas.preusser@xilinx.com>
2022-09-09 15:52:46 +0100

    Reimplement with II=1 and associated testbench.

--- kernels/plugins/client_arbiter/client_arbiter.cpp
+++ kernels/plugins/client_arbiter/client_arbiter.cpp
@@ -17,6 +17,7 @@
 
 #include "client_arbiter.h"
 
+
 void client_arbiter(
 	STREAM<command_word> cmd_clients[NUM_CTRL_STREAMS],
 	STREAM<command_word> ack_clients[NUM_CTRL_STREAMS],
@@ -34,43 +35,46 @@ void client_arbiter(
 	// 1-Hot indication of current token position of highest priority
 	static ap_uint<NUM_CTRL_STREAMS>  pos = 1;
 #pragma HLS reset variable=pos
+	static ap_uint<4>  cnt = 0;
+#pragma HLS reset variable=cnt
 
-	// Request vector with asserted added MSB
-	ap_uint<1+NUM_CTRL_STREAMS>  rqst;
-	for(int  i = 0; i < NUM_CTRL_STREAMS; i++) {
-#pragma HLS unroll
-		rqst[i] = !STREAM_IS_EMPTY(cmd_clients[i]);
-	}
-	rqst[NUM_CTRL_STREAMS] = 1;
-
-	// 1-Hot grant vectors starting at token position and at LSB, respectively
-	ap_uint<1+NUM_CTRL_STREAMS> const  grnt1 = (~rqst + pos) & rqst;
-	ap_uint<1+NUM_CTRL_STREAMS> const  grnt0 = (~rqst +   1) & rqst;
-
-	// Take this round only if there is at least one request
-	if(!grnt0[NUM_CTRL_STREAMS]) {
-		ap_uint<NUM_CTRL_STREAMS> const  grant = grnt1[NUM_CTRL_STREAMS]? grnt0 : grnt1;
-
-		// Collect the granted command
-		ap_uint<32>  cmd = 0;
+	if(cnt != 0) {
+		command_word  cmd;
 		for(int  i = 0; i < NUM_CTRL_STREAMS; i++) {
-			if(grant[i]) {
-				// Dispatch to CCLO
-				for(int j=0; j<15; j++){
-					STREAM_WRITE(cmd_cclo, STREAM_READ(cmd_clients[i]));
-				}
+#pragma HLS unroll
+			if(pos[i])  cmd = STREAM_READ(cmd_clients[i]);
+		}
+		STREAM_WRITE(cmd_cclo, cmd);
+
+		if(--cnt == 0) {
+			// Return ACK
+			command_word  const ack = STREAM_READ(ack_cclo);
+			for(int  i = 0; i < NUM_CTRL_STREAMS; i++) {
+#pragma HLS unroll
+				if(pos[i])  STREAM_WRITE(ack_clients[i], ack);
 			}
+
+			// Rotate priority position to left neighbor
+			pos = (pos(NUM_CTRL_STREAMS-2, 0), pos[NUM_CTRL_STREAMS-1]);
 		}
-
-		command_word const ack = STREAM_READ(ack_cclo);
-
-		// Return ACK
+	}
+	else {
+		// Request vector with asserted added MSB
+		ap_uint<1+NUM_CTRL_STREAMS>  rqst;
 		for(int  i = 0; i < NUM_CTRL_STREAMS; i++) {
 #pragma HLS unroll
-			if(grant[i])  STREAM_WRITE(ack_clients[i], ack);
+			rqst[i] = !STREAM_IS_EMPTY(cmd_clients[i]);
 		}
+		rqst[NUM_CTRL_STREAMS] = 1;
 
-		// Rotate priority position to left neighbor
-		pos = (grant(NUM_CTRL_STREAMS-2, 0), grant[NUM_CTRL_STREAMS-1]);
+		// 1-Hot grant vectors starting at token position and at LSB, respectively
+		ap_uint<1+NUM_CTRL_STREAMS> const  grnt1 = (~rqst + pos) & rqst;
+		ap_uint<1+NUM_CTRL_STREAMS> const  grnt0 = (~rqst +   1) & rqst;
+
+		// Commit to highest-prio request if any
+		if(!grnt0[NUM_CTRL_STREAMS]) {
+			pos = grnt1[NUM_CTRL_STREAMS]? grnt0 : grnt1;
+			cnt = 15;
+		}
 	}
 }
