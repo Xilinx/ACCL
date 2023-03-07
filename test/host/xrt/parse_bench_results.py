4dde92e63ec6b85693512eeeff790d3e0fdcd565
Lucian Petrica <lucianp@xilinx.com>
2023-03-07 12:04:43 +0000

    Updated benchmark results parser

--- test/host/xrt/parse_bench_results.py
+++ test/host/xrt/parse_bench_results.py
@@ -20,129 +20,110 @@ import json
 import numpy
 import math
 import matplotlib.pyplot as plt
-import statistics
+import pandas as pd
 
 from pathlib import Path
 from typing import Dict, List, Tuple, Union
+from enum import IntEnum
 
+class Opcode(IntEnum):
+    CONFIG         = 0
+    COPY           = 1
+    COMBINE        = 2
+    SEND           = 3 
+    RECV           = 4
+    BCAST          = 5
+    SCATTER        = 6
+    GATHER         = 7
+    REDUCE         = 8
+    ALLGATHER      = 9
+    ALLREDUCE      = 10
+    REDUCE_SCATTER = 11
 
-def _ideal(times: List[float], rtt: float) -> List[float]:
-    return [time - rtt / 2 for time in times]
+# TODOs:
+# - parse benchmark CSV(s) and plot duration vs message size, including the stddev
+# - calculate bandwidth efficiency compared to ideal theoretical for each collective
+#    - script takes world size and local rank as input; operation root is in the CSV
+#    - one model per each primitive collective; model may differ based on where root is
+#    - script takes frequency as input to convert cycles to microseconds
 
-
-def _bw(times: List[float], size: int) -> List[float]:
-    return [(size * 8) / time for time in times]
-
-
-def _stats(data: List[float]) -> Tuple[float, float]:
-        mean = statistics.mean(data)
-        dev = statistics.stdev(data, mean)
-        return mean, dev
-
+def ideal_duration(opcode, world_size, msg_size_bits, fhz, rtt_s, bw_bps):
+    if(opcode == Opcode.SEND):
+        duration = msg_size_bits/bw_bps
+    elif(opcode == Opcode.BCAST):
+        duration = (world_size-1)*(msg_size_bits/bw_bps)
+    elif(opcode == Opcode.SCATTER):
+        duration = ((msg_size_bits/world_size)/bw_bps)
+    elif(opcode == Opcode.GATHER):
+        duration = world_size*((rtt_s/2)+msg_size_bits/bw_bps)
+    elif(opcode == Opcode.REDUCE):
+        duration = world_size*((rtt_s/2)+msg_size_bits/bw_bps)
+    elif(opcode == Opcode.ALLGATHER):
+        duration = (world_size-1)*((rtt_s/2)+(msg_size_bits/world_size)/bw_bps)
+    elif(opcode == Opcode.ALLREDUCE):
+        duration = 2*(world_size-1)*((rtt_s/2)+(msg_size_bits/world_size)/bw_bps)
+    elif(opcode == Opcode.REDUCE_SCATTER):
+        duration = (world_size-1)*((rtt_s/2)+(msg_size_bits/world_size)/bw_bps)
+    else:
+        duration = 0
+    return duration
 
 class ResultsParser:
-    def __init__(self, input_file: Path) -> None:
-        with input_file.open('r') as input:
-            self.data: dict = json.load(input)
-        self.latencies: List[float] = self.data['latency']
-        self.rtts: List[float] = self.data['rtt']
-        self.sizes: List[int] = sorted(int(k) * 4 for k in self.data['throughput'])
-        self.throughputs: Dict[int, List[float]] = \
-            {int(k) * 4: v for k, v in self.data['throughput'].items()}
-        self.allreduces: Dict[int, List[float]] = \
-            {int(k) * 4: v for k, v in self.data['allreduce'].items()}
+    def __init__(self, input_file, fmhz, size, rtt_ns, bw_gbps) -> None:
+        results = pd.read_csv(input_file)
+        results['duration_us'] = results['Cycles'] / fmhz
+        results['msg_size_bytes'] = results['Count'] * 4
+        self.op_results = {}
+        for opcode in Opcode:
+            if opcode == Opcode.CONFIG: continue
+            if opcode == Opcode.COPY: continue
+            if opcode == Opcode.COMBINE: continue
+            if opcode == Opcode.RECV: continue
+            tmp_df = results[results['Opcode'] == opcode].groupby('msg_size_bytes').agg({'duration_us':['mean', 'std']})
+            tmp_df.columns = ['duration_us_mean', 'duration_us_stddev']
+            tmp_df = tmp_df.reset_index()
+            tmp_df['ideal_duration_us'] = tmp_df.apply(lambda x : ideal_duration(opcode, size, x.msg_size_bytes*8, fmhz*(10**6), rtt_ns/(10**9), bw_gbps*(10**9))*(10**6), axis=1)
+            tmp_df['duration_ratio'] = tmp_df['duration_us_mean'] / tmp_df['ideal_duration_us']
+            self.op_results[opcode] = tmp_df
 
-    @property
-    def latency(self) -> Tuple[float, float]:
-        return _stats(self.latencies)
+    def print(self, path):
+        with open(path+'/results.md', 'w') as f:
+            for opcode in Opcode:
+                try:
+                    tmp_str = '\n\n' + opcode.name + '\n===========================\n\n'
+                    tmp_str += self.op_results[opcode].to_markdown(index=False, floatfmt=(".0f", ".2f", ".2f", ".2f", ".0f"))
+                    f.write(tmp_str)
+                except:
+                    continue
 
-    @property
-    def rtt(self) -> Tuple[float, float]:
-        return _stats(self.rtts)
-
-    @property
-    def send_recv(self) -> Dict[int, Tuple[float, float]]:
-        return {size: _stats(self.throughputs[size])
-                for size in self.sizes}
-
-    @property
-    def allreduce(self) -> Dict[int, Tuple[float, float]]:
-        return {size: _stats(self.allreduces[size])
-                for size in self.sizes}
-
-    @property
-    def send_recv_bw(self) -> Dict[int, Tuple[float, float]]:
-        return {size: _stats(_bw(_ideal(self.throughputs[size], self.rtt[0] - self.rtt[1]), size))
-                for size in self.sizes}
-
-    @property
-    def allreduce_bw(self) -> Dict[int, Tuple[float, float]]:
-        return {size: _stats(_bw(_ideal(self.allreduces[size], self.rtt[0] - self.rtt[1]), size))
-                for size in self.sizes}
-
-
-def print_results(results: ResultsParser):
-    def data_unit(size: Union[int, float], bw: bool = False) -> str:
-        pre = 'bps' if bw else 'B'
-        if size < 1e2:
-            return f"{round(size)} {pre}"
-        elif size < 1e4:
-            return f"{size / 1e3:.2f} k{pre}"
-        elif size < 1e5:
-            return f"{size / 1e3:.1f} k{pre}"
-        elif size < 1e7:
-            return f"{size / 1e6:.2f} M{pre}"
-        elif size < 1e8:
-            return f"{size / 1e6:.1f} M{pre}"
-        elif size < 1e10:
-            return f"{size / 1e9:.2f} G{pre}"
-        else:
-            return f"{size / 1e9:.1f} G{pre}"
-
-    def time_unit(time: float) -> str:
-        if time >= 1e2:
-            return f"{round(time):d} s"
-        elif time >= 1e1:
-            return f"{time:.1f} s"
-        elif time >= 1e-1:
-            return f"{time:.2f} s"
-        elif time >= 1e-2:
-            return f"{time * 1e3:.1f} ms"
-        elif time >= 1e-4:
-            return f"{time * 1e3:.2f} ms"
-        elif time >= 1e-5:
-            return f"{time * 1e6:.1f} μs"
-        elif time >= 1e-7:
-            return f"{time * 1e6:.2f} μs"
-        elif time >= 1e-8:
-            return f"{time * 1e9:.2f} ns"
-        elif time >= 1e-10:
-            return f"{time * 1e9:.2f} ns"
-        else:
-            return f"{time:.3e} s"
-
-    print(f"Latency: {time_unit(results.latency[0])} ± "
-          f"{time_unit(results.latency[1])}")
-    print(f"RTT: {time_unit(results.rtt[0])} ± {time_unit(results.rtt[1])}")
-    print("Throughput: ",
-          {f"{data_unit(size)}":
-           f"{data_unit(through[0], True)} ± {data_unit(through[1], True)}"
-           for size, through in results.send_recv_bw.items()})
-    print("Allreduce: ",
-          {f"{data_unit(size)}": f"{data_unit(allred[0], True)} ± {data_unit(allred[1], True)}"
-           for size, allred in results.allreduce_bw.items()})
+    def plot(self, path):
+        for opcode in Opcode:
+            try:
+                self.op_results[opcode].plot(legend='', x='msg_size_bytes', y='duration_us_mean', yerr='duration_us_stddev', loglog=True)
+            except:
+                continue
+            plt.xlabel('Message size (bytes)')
+            plt.ylabel('Duration (us)')
+            plt.title(opcode.name)
+            plt.savefig(path+'/plot_'+str(opcode.name)+'.pdf')
 
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Parse benchmark results')
-    parser.add_argument('-i', '--input-file', type=Path,
-                        default='results-1.json', metavar='JSON',
-                        help="Input JSON file (defaults to '%(default)s')")
-    parser.add_argument('-o', '--output-directory', type=Path,
-                        default='results/', metavar='DIR',
+    parser.add_argument('-i', '--input-file', type=str, required = True, help="Input CSV file")
+    parser.add_argument('-o', '--output-directory', type=str, default='.', 
                         help="Output directory (defaults to '%(default)s')")
+    parser.add_argument('-l', '--rtt_ns', type=int, default=500,
+                        help="Round trip latency, in nanoseconds (defaults to '%(default)s')")
+    parser.add_argument('-f', '--fmhz', type=int, default=250,
+                        help="Operating frequency, in megahertz (defaults to '%(default)s')")
+    parser.add_argument('-s', '--size', type=int, required=True, help="World size")
+    parser.add_argument('-b', '--bw_gbps', type=int, default=100, help="Network bandwidth, in Gbps (defaults to '%(default)s')")
+    parser.add_argument('-p', '--plot', action='store_true', help="Enable plotting")
     args = parser.parse_args()
 
-    results = ResultsParser(args.input_file)
-    print_results(results)
+    results = ResultsParser(args.input_file, args.fmhz, args.size, args.rtt_ns, args.bw_gbps)
+    if args.plot:
+        results.plot(args.output_directory)
+        results.print(args.output_directory)
