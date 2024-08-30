ece5cd6f7778cc3642fdc574372d07a96160837c
zhe <zhe@ethz.ch>
2024-08-30 17:38:20 +0200

    update coyote submodule to latest commit

--- kernels/plugins/cyt_adapter/cyt_dma_adapter.cpp
+++ kernels/plugins/cyt_adapter/cyt_dma_adapter.cpp
@@ -19,9 +19,15 @@
 
 using namespace std;
 
+// convert the data mover command (dma) to the cyt_req_t (dma)
+// currently all the memory accesses initialized by the CCLO is associated with pid 0 (coyote_proc)
+// also we assume a vfid 0 for single cyt region
+// the dest field of the dm cmd indicates the host/card accesses
+// the dest field is converted to strm flag in the cyt_sq_cmd
+// DMA Channel is used to select axis streams, channel 0 and 1 are reserved 
 template <int DMA_CHANNEL>
-void dm_byp_cmd_converter(hls::stream<ap_axiu<104,0,0,DEST_WIDTH>>& dm_cmd,
-							hls::stream<cyt_req_t>& byp_cmd,
+void dm_sq_cmd_converter(hls::stream<ap_axiu<104,0,0,DEST_WIDTH>>& dm_cmd,
+							hls::stream<cyt_req_t>& cyt_sq_cmd,
 							hls::stream<ap_uint<1+4+23>>& dm_meta
 							)
 {
@@ -39,8 +45,12 @@ void dm_byp_cmd_converter(hls::stream<ap_axiu<104,0,0,DEST_WIDTH>>& dm_cmd,
 		ap_uint<1> strm = dm_cmd_with_dest.dest(2,0); // 1 if targeting host memory, 0 if targeting card memory
 		ap_uint<1> ctl = dm_cmd_word(30,30); // ctl field determines if a TLAST must be asserted at the end of the data stream
 
-		cyt_req_t req(0, 0, 0, DMA_CHANNEL, 0, ctl, 0, strm, btt, saddr);
-		STREAM_WRITE(byp_cmd, req);
+		cyt_req_t req(0/*rsrvd_arg*/, 0 /*offs_arg*/, 0 /*host_arg*/, 0 /*actv_arg*/,
+              btt/*len_arg*/, saddr /*vaddr_arg*/, ctl /*last_arg*/,
+              DMA_CHANNEL /*dest_arg*/, 0 /*pid_arg*/, 0 /*vfid_arg*/,
+              0 /*remote_arg*/, 0 /*rdma_arg*/, 0 /*mode_arg*/, strm /*strm_arg*/, 0 /*opcode_arg*/);
+
+		STREAM_WRITE(cyt_sq_cmd, req);
 
 		ap_uint<1+4+23> dm_meta_word;
 		dm_meta_word(22,0) = btt;
@@ -50,23 +60,33 @@ void dm_byp_cmd_converter(hls::stream<ap_axiu<104,0,0,DEST_WIDTH>>& dm_cmd,
 	}
 }
 
+// convert the cyt_rq (rdma) to cyt_sq (dma)
+// Channel 2 of the host/card axis stream is reserved for cyt_rq command
+// the rq dest field is used to indicate whether this is host/device access, it should be converted to strm field here
+// the sq opcode is not relevant as it is targeting dma
 template <int DMA_CHANNEL>
-void rdma_req_byp_cmd_converter(
-						hls::stream<cyt_req_t>& rdma_req,
-						hls::stream<cyt_req_t>& byp_cmd
+void cyt_rq_sq_cmd_converter(
+						hls::stream<cyt_req_t>& cyt_rq_cmd,
+						hls::stream<cyt_req_t>& cyt_sq_cmd
 )
 {
 #pragma HLS inline off
 #pragma HLS pipeline II=1
 
-	if(!STREAM_IS_EMPTY(rdma_req)){
-		cyt_req_t req = STREAM_READ(rdma_req);
-		// TODO:
-		// Better mechanism of buffer & proc mapping 
+	if(!STREAM_IS_EMPTY(cyt_rq_cmd)){
+		cyt_req_t req = STREAM_READ(cyt_rq_cmd);
+		
 		// Currently has to set the pid to 0, corresponding to coyote_proc instead of any coyote_qproc
-		// Every coyote_qproc has a unique physical address in device
-		cyt_req_t cmd(req.rsrvd, req.vfid, 0 /*req.pid*/, DMA_CHANNEL, 0, 1, 0, req.stream, req.len, req.vaddr);
-		STREAM_WRITE(byp_cmd, cmd);
+		// Because all the buffer allocation within the ACCL driver is associated with the coyote_proc
+		// And every coyote_qproc has a unique physical address in device which is different than the coyote_proc
+		// Also mark the host flag in the new output command to 0 to indicate the command is issued from the kernel instead of host
+		// However, the cq of this command is not processed in the cq_dm_sts_converter as the dest channel is 2
+		cyt_req_t cmd(req.rsrvd/*rsrvd_arg*/, req.offs /*offs_arg*/, 0/*host_arg*/, req.actv /*actv_arg*/,
+              req.len/*len_arg*/, req.vaddr /*vaddr_arg*/, req.last /*last_arg*/,
+              DMA_CHANNEL /*dest_arg*/, 0 /*pid_arg*/, req.vfid /*vfid_arg*/,
+              req.remote /*remote_arg*/, req.rdma /*rdma_arg*/, req.mode /*mode_arg*/, req.dest /*strm_arg*/, req.opcode /*opcode_arg*/);
+
+		STREAM_WRITE(cyt_sq_cmd, cmd);
 	}
 
 }
@@ -74,6 +94,7 @@ void rdma_req_byp_cmd_converter(
 void multiplexor(hls::stream<cyt_req_t>& in0,
 				hls::stream<cyt_req_t>& in1,
 				hls::stream<cyt_req_t>& in2,
+				hls::stream<cyt_req_t>& in3,
 				hls::stream<cyt_req_t>& out)
 {
 #pragma HLS inline off
@@ -96,11 +117,16 @@ void multiplexor(hls::stream<cyt_req_t>& in0,
 		currWord = STREAM_READ(in2);
 		STREAM_WRITE(out, currWord);
 	}
+	else if(!STREAM_IS_EMPTY(in3))
+	{
+		currWord = STREAM_READ(in3);
+		STREAM_WRITE(out, currWord);
+	}
 
 }
 
 
-void byp_dm_sts_converter(hls::stream<ap_uint<16>> & byp_sts, 
+void cq_dm_sts_converter(hls::stream<cyt_ack_t> & cq_sts, 
 						hls::stream<ap_axiu<32,0,0,0>> & dm0_sts,
 						hls::stream<ap_axiu<32,0,0,0>> & dm1_sts,
 						hls::stream<ap_uint<1+4+23>>& dm0_meta,
@@ -109,49 +135,48 @@ void byp_dm_sts_converter(hls::stream<ap_uint<16>> & byp_sts,
 #pragma HLS inline off
 #pragma HLS pipeline II=1
 
-	if (!STREAM_IS_EMPTY(byp_sts))
+	if (!STREAM_IS_EMPTY(cq_sts))
 	{
-		ap_uint<16> byp_sts_word = STREAM_READ(byp_sts);
-		// PID in LSB according to Coyote dma_rsp_t:
-		ap_uint<CYT_PID_BITS> pid = byp_sts_word(CYT_PID_BITS-1,0);
-		ap_uint<CYT_DEST_BITS> dest = byp_sts_word(CYT_DEST_BITS+CYT_PID_BITS-1,CYT_PID_BITS);
-		ap_uint<1> strm = byp_sts_word(CYT_DEST_BITS+CYT_PID_BITS,CYT_DEST_BITS+CYT_PID_BITS);
-		ap_uint<1> host = byp_sts_word(CYT_DEST_BITS+CYT_PID_BITS+1,CYT_DEST_BITS+CYT_PID_BITS+1);
-		
+		cyt_ack_t cq_sts_word = STREAM_READ(cq_sts);
+
 		ap_axiu<32,0,0,0> dm_sts_word;
 		ap_uint<1+4+23> dm_meta_word;
 
-		// only send back ack when the byp_sts stems from kernel issued bypass commands
-		// if dest == 2, this comes from wr_req/rd_req, no need to forward to data mover
-		if(host == 0)
+		// only process status if it is local memory completion status
+		if(cq_sts_word.opcode == CYT_STRM_CARD || cq_sts_word.opcode == CYT_STRM_HOST)
 		{
-			do{
-				if(dest == 0){
-					dm_meta_word = STREAM_READ(dm0_meta);
-				} else if (dest == 1){
-					dm_meta_word = STREAM_READ(dm1_meta);
-				}
-				dm_sts_word.data.range(3,0) = dm_meta_word(26,23); //tag
-				dm_sts_word.data.range(4,4) = 0; // internal error
-				dm_sts_word.data.range(5,5) = 0; // decode error
-				dm_sts_word.data.range(6,6) = 0; // slave error
-				dm_sts_word.data.range(7,7) = 1; // OK
-				dm_sts_word.data.range(30,8) = dm_meta_word(22,0); // bytes received
-				dm_sts_word.data.range(31,31) = dm_meta_word(27,27); // EOP
-				dm_sts_word.last = 1;
-				if(dest == 0){
-					STREAM_WRITE(dm0_sts, dm_sts_word);
-				} else if (dest == 1){
-					STREAM_WRITE(dm1_sts, dm_sts_word);
-				}
-			} while(dm_meta_word(27,27) == 0);
+			// only send back ack when the cq_sts stems from kernel issued bypass commands
+			// if dest == 2, this comes from wr_req/rd_req, no need to forward to data mover
+			if(cq_sts_word.host == 0)
+			{
+				do{
+					if(cq_sts_word.dest == 0){
+						dm_meta_word = STREAM_READ(dm0_meta);
+					} else if (cq_sts_word.dest == 1){
+						dm_meta_word = STREAM_READ(dm1_meta);
+					}
+					dm_sts_word.data.range(3,0) = dm_meta_word(26,23); //tag
+					dm_sts_word.data.range(4,4) = 0; // internal error
+					dm_sts_word.data.range(5,5) = 0; // decode error
+					dm_sts_word.data.range(6,6) = 0; // slave error
+					dm_sts_word.data.range(7,7) = 1; // OK
+					dm_sts_word.data.range(30,8) = dm_meta_word(22,0); // bytes received
+					dm_sts_word.data.range(31,31) = dm_meta_word(27,27); // EOP
+					dm_sts_word.last = 1;
+					if(cq_sts_word.dest == 0){
+						STREAM_WRITE(dm0_sts, dm_sts_word);
+					} else if (cq_sts_word.dest == 1){
+						STREAM_WRITE(dm1_sts, dm_sts_word);
+					}
+				} while(dm_meta_word(27,27) == 0);
+			}
 		}
 	}
 
 }
 
-// The cyt bypass commands have 3 sources if RDMA is enabled
-// 2 DMA channels from the CCLO and the rdma req interface
+// The cyt sq commands have 4 sources if RDMA is enabled
+// 2 DMA channels from the CCLO, CCLO sq command, and the Cyt rq interface
 void cyt_dma_adapter(
 	//DM command streams
 	hls::stream<ap_axiu<104,0,0,DEST_WIDTH>> &dma0_s2mm_cmd,
@@ -164,15 +189,20 @@ void cyt_dma_adapter(
 	hls::stream<ap_axiu<32,0,0,0>> &dma0_mm2s_sts,
 	hls::stream<ap_axiu<32,0,0,0>> &dma1_mm2s_sts,
 
-	//RDMA rd_req and wr_req
-	hls::stream<cyt_req_t> & rdma_wr_req,
-	hls::stream<cyt_req_t> & rdma_rd_req,
+	//Coyote rq rd_req and wr_req
+	hls::stream<cyt_req_t> & cyt_rq_wr_cmd,
+	hls::stream<cyt_req_t> & cyt_rq_rd_cmd,
 
-	//Coyote Bypass interface command and status
-	hls::stream<cyt_req_t> &cyt_byp_wr_cmd,
-	hls::stream<ap_uint<16>> &cyt_byp_wr_sts,
-	hls::stream<cyt_req_t> &cyt_byp_rd_cmd,
-	hls::stream<ap_uint<16>> &cyt_byp_rd_sts
+	//CCLO sq command
+	hls::stream<cyt_req_t >& cclo_sq_wr_cmd,
+	hls::stream<cyt_req_t >& cclo_sq_rd_cmd,
+
+	//Coyote sq interface command and cq status
+	hls::stream<cyt_req_t> &cyt_sq_wr_cmd,
+	hls::stream<cyt_req_t> &cyt_sq_rd_cmd,
+
+	hls::stream<cyt_ack_t> &cyt_cq_wr_sts,
+	hls::stream<cyt_ack_t> &cyt_cq_rd_sts
 ) {
 #pragma HLS INTERFACE axis port=dma0_s2mm_cmd
 #pragma HLS INTERFACE axis port=dma1_s2mm_cmd
@@ -182,29 +212,36 @@ void cyt_dma_adapter(
 #pragma HLS INTERFACE axis port=dma1_s2mm_sts
 #pragma HLS INTERFACE axis port=dma0_mm2s_sts
 #pragma HLS INTERFACE axis port=dma1_mm2s_sts
-#pragma HLS INTERFACE axis port=cyt_byp_rd_cmd
-#pragma HLS INTERFACE axis port=cyt_byp_rd_sts
-#pragma HLS INTERFACE axis port=cyt_byp_wr_cmd
-#pragma HLS INTERFACE axis port=cyt_byp_wr_sts
+#pragma HLS INTERFACE axis port=cyt_sq_rd_cmd
+#pragma HLS INTERFACE axis port=cyt_cq_rd_sts
+#pragma HLS INTERFACE axis port=cyt_sq_wr_cmd
+#pragma HLS INTERFACE axis port=cyt_cq_wr_sts
 #pragma HLS INTERFACE ap_ctrl_none port=return
 #pragma HLS DATAFLOW disable_start_propagation
 
-#pragma HLS aggregate variable=cyt_byp_wr_cmd compact=bit
-#pragma HLS aggregate variable=cyt_byp_rd_cmd compact=bit
+#pragma HLS aggregate variable=cyt_sq_wr_cmd compact=bit
+#pragma HLS aggregate variable=cyt_sq_rd_cmd compact=bit
+#pragma HLS aggregate variable=cyt_cq_wr_sts compact=bit
+#pragma HLS aggregate variable=cyt_cq_rd_sts compact=bit
 
-#pragma HLS INTERFACE axis port=rdma_wr_req
-#pragma HLS INTERFACE axis port=rdma_rd_req
-#pragma HLS aggregate variable=rdma_wr_req compact=bit
-#pragma HLS aggregate variable=rdma_rd_req compact=bit
+#pragma HLS INTERFACE axis port=cyt_rq_wr_cmd
+#pragma HLS INTERFACE axis port=cyt_rq_rd_cmd
+#pragma HLS aggregate variable=cyt_rq_wr_cmd compact=bit
+#pragma HLS aggregate variable=cyt_rq_rd_cmd compact=bit
 
-	static hls::stream<cyt_req_t > byp_wr_cmd_0;
-    #pragma HLS stream variable=byp_wr_cmd_0 depth=16
-	static hls::stream<cyt_req_t > byp_wr_cmd_1;
-    #pragma HLS stream variable=byp_wr_cmd_1 depth=16
-	static hls::stream<cyt_req_t > byp_rd_cmd_0;
-    #pragma HLS stream variable=byp_rd_cmd_0 depth=16
-	static hls::stream<cyt_req_t > byp_rd_cmd_1;
-    #pragma HLS stream variable=byp_rd_cmd_1 depth=16
+#pragma HLS INTERFACE axis port=cclo_sq_wr_cmd
+#pragma HLS aggregate variable=cclo_sq_wr_cmd compact=bit
+#pragma HLS INTERFACE axis port=cclo_sq_rd_cmd
+#pragma HLS aggregate variable=cclo_sq_rd_cmd compact=bit
+
+	static hls::stream<cyt_req_t > sq_wr_cmd_0;
+    #pragma HLS stream variable=sq_wr_cmd_0 depth=16
+	static hls::stream<cyt_req_t > sq_wr_cmd_1;
+    #pragma HLS stream variable=sq_wr_cmd_1 depth=16
+	static hls::stream<cyt_req_t > sq_rd_cmd_0;
+    #pragma HLS stream variable=sq_rd_cmd_0 depth=16
+	static hls::stream<cyt_req_t > sq_rd_cmd_1;
+    #pragma HLS stream variable=sq_rd_cmd_1 depth=16
 
 	static hls::stream<ap_uint<1+4+23>> dma0_mm2s_meta;
     #pragma HLS stream variable=dma0_mm2s_meta depth=16
@@ -215,24 +252,25 @@ void cyt_dma_adapter(
 	static hls::stream<ap_uint<1+4+23>> dma1_s2mm_meta;
     #pragma HLS stream variable=dma1_s2mm_meta depth=16
 
-	static hls::stream<cyt_req_t > byp_wr_cmd_2;
-    #pragma HLS stream variable=byp_wr_cmd_2 depth=16
-	static hls::stream<cyt_req_t > byp_rd_cmd_2;
-    #pragma HLS stream variable=byp_rd_cmd_2 depth=16
+	static hls::stream<cyt_req_t > sq_wr_cmd_2;
+    #pragma HLS stream variable=sq_wr_cmd_2 depth=16
+	static hls::stream<cyt_req_t > sq_rd_cmd_2;
+    #pragma HLS stream variable=sq_rd_cmd_2 depth=16
 
-	dm_byp_cmd_converter<0>(dma0_s2mm_cmd, byp_wr_cmd_0, dma0_s2mm_meta);
-	dm_byp_cmd_converter<1>(dma1_s2mm_cmd, byp_wr_cmd_1, dma1_s2mm_meta);
-	rdma_req_byp_cmd_converter<2>(rdma_wr_req, byp_wr_cmd_2);
-	multiplexor(byp_wr_cmd_0,byp_wr_cmd_1,byp_wr_cmd_2,cyt_byp_wr_cmd);
+	dm_sq_cmd_converter<0>(dma0_s2mm_cmd, sq_wr_cmd_0, dma0_s2mm_meta);
+	dm_sq_cmd_converter<1>(dma1_s2mm_cmd, sq_wr_cmd_1, dma1_s2mm_meta);
+	cyt_rq_sq_cmd_converter<2>(cyt_rq_wr_cmd, sq_wr_cmd_2);
+	multiplexor(cclo_sq_wr_cmd, sq_wr_cmd_0,sq_wr_cmd_1,sq_wr_cmd_2, cyt_sq_wr_cmd);
 
 
-	dm_byp_cmd_converter<0>(dma0_mm2s_cmd,byp_rd_cmd_0, dma0_mm2s_meta);
-	dm_byp_cmd_converter<1>(dma1_mm2s_cmd,byp_rd_cmd_1, dma1_mm2s_meta);
-	rdma_req_byp_cmd_converter<2>(rdma_rd_req, byp_rd_cmd_2);
-	multiplexor(byp_rd_cmd_0,byp_rd_cmd_1,byp_rd_cmd_2,cyt_byp_rd_cmd);
+	dm_sq_cmd_converter<0>(dma0_mm2s_cmd,sq_rd_cmd_0, dma0_mm2s_meta);
+	dm_sq_cmd_converter<1>(dma1_mm2s_cmd,sq_rd_cmd_1, dma1_mm2s_meta);
+	cyt_rq_sq_cmd_converter<2>(cyt_rq_rd_cmd, sq_rd_cmd_2);
+	multiplexor(cclo_sq_rd_cmd, sq_rd_cmd_0,sq_rd_cmd_1,sq_rd_cmd_2, cyt_sq_rd_cmd);
 
-	byp_dm_sts_converter(cyt_byp_wr_sts, dma0_s2mm_sts, dma1_s2mm_sts, dma0_s2mm_meta, dma1_s2mm_meta);
-	byp_dm_sts_converter(cyt_byp_rd_sts, dma0_mm2s_sts, dma1_mm2s_sts, dma0_mm2s_meta, dma1_mm2s_meta);
+	// handle the completion queue conversion to dm sts
+	cq_dm_sts_converter(cyt_cq_wr_sts, dma0_s2mm_sts, dma1_s2mm_sts, dma0_s2mm_meta, dma1_s2mm_meta);
+	cq_dm_sts_converter(cyt_cq_rd_sts, dma0_mm2s_sts, dma1_mm2s_sts, dma0_mm2s_meta, dma1_mm2s_meta);
 
 
 }
--- kernels/plugins/cyt_adapter/cyt_rdma_arbiter.cpp
+++ kernels/plugins/cyt_adapter/cyt_rdma_arbiter.cpp
@@ -38,23 +38,23 @@ void cyt_rdma_arbiter_meta(
 
     if (!STREAM_IS_EMPTY(s_meta)){
 		reqWord = STREAM_READ(s_meta);
-        if (reqWord.host == 0){
+        if (reqWord.strm == CYT_STRM_RDMA && reqWord.opcode == CYT_RDMA_SEND){
             meta_notif.type = 0; //don't care
             meta_notif.session_id(CYT_PID_BITS-1,0) = reqWord.pid;
-            meta_notif.session_id(CYT_PID_BITS+CYT_DEST_BITS-1,CYT_PID_BITS) = reqWord.dest;
+            meta_notif.session_id(CYT_PID_BITS+CYT_DEST_BITS-1,CYT_PID_BITS) = reqWord.vfid; //TODO: check this
             meta_notif.length = reqWord.len;
             STREAM_WRITE(m_meta_0, meta_notif); 
 
-            meta_internal(15,0) = reqWord.host;
-            meta_internal(31,16) = reqWord.stream;
+            meta_internal(15,0) = reqWord.dest;
+            meta_internal(31,16) = CYT_RDMA_SEND;
             meta_internal(63,32) = reqWord.len;
             STREAM_WRITE(meta_int, meta_internal);
 
-        } else if (reqWord.host == 1) {
+        } else if (reqWord.strm == CYT_STRM_RDMA && reqWord.opcode == CYT_RDMA_WRITE) {
+            // simply forward the rq_wr and the conversion from rq_wr to sq_wr is done downstream
             STREAM_WRITE(m_meta_1, reqWord);
-
-            meta_internal(15,0) = reqWord.host;
-            meta_internal(31,16) = reqWord.stream;
+            meta_internal(15,0) = reqWord.dest;
+            meta_internal(31,16) = CYT_RDMA_WRITE;
             meta_internal(63,32) = reqWord.len;
             STREAM_WRITE(meta_int, meta_internal);
         }
@@ -68,7 +68,8 @@ void cyt_rdma_arbiter_meta(
 // We also append the last signal for WRITE data stream for each packet as the cyt adapter set the ctl bits always to 1
 void cyt_rdma_arbiter_data(	
                 hls::stream<ap_uint<64> >& meta_int,
-                hls::stream<ap_axiu<512, 0, 0, 8> >& s_axis,
+                hls::stream<ap_axiu<512, 0, 0, 8> >& s_axis_0,
+                hls::stream<ap_axiu<512, 0, 0, 8> >& s_axis_1,
                 hls::stream<ap_axiu<512, 0, 0, 8> >& m_axis_0,
                 hls::stream<ap_axiu<512, 0, 0, 8> >& m_axis_1
 )
@@ -76,14 +77,14 @@ void cyt_rdma_arbiter_data(
 	#pragma HLS PIPELINE II=1
 	#pragma HLS INLINE off
 
-	enum fsmStateType {META, SEND_STREAM, WRITE_STREAM};
+	enum fsmStateType {META, SEND_STREAM, WRITE_STREAM_CARD, WRITE_STREAM_HOST};
     static fsmStateType  fsmState = META;
 
     static ap_axiu<512, 0, 0, 8> currWord;
 
     static ap_uint<64> meta_internal;
     static ap_uint<16> meta_internal_host;
-    static ap_uint<16> meta_internal_stream;
+    static ap_uint<16> meta_internal_opcode;
     static ap_uint<32> meta_internal_len;
     static ap_uint<32> pkt_word;
     static ap_uint<32> word_cnt = 0;
@@ -95,22 +96,24 @@ void cyt_rdma_arbiter_data(
         {
             meta_internal = STREAM_READ(meta_int);
             meta_internal_host = meta_internal(15,0);
-            meta_internal_stream = meta_internal(31,16);
+            meta_internal_opcode = meta_internal(31,16);
             meta_internal_len = meta_internal(63,32);
 
             pkt_word = (meta_internal_len + 63) >> 6;
 
-            if (meta_internal_host == 0){
+            if (meta_internal_opcode == CYT_RDMA_SEND){
                 fsmState = SEND_STREAM;
-            } else if (meta_internal_host == 1){
-                fsmState = WRITE_STREAM;
+            } else if (meta_internal_opcode == CYT_RDMA_WRITE && meta_internal_host == CYT_STRM_CARD){
+                fsmState = WRITE_STREAM_CARD;
+            } else if (meta_internal_opcode == CYT_RDMA_WRITE && meta_internal_host == CYT_STRM_HOST){
+                fsmState = WRITE_STREAM_HOST;
             }
         }
         break;
     case SEND_STREAM:
-        if (!s_axis.empty())
+        if (!s_axis_0.empty())
         {
-            currWord = STREAM_READ(s_axis);
+            currWord = STREAM_READ(s_axis_0);
             word_cnt++;
             if (word_cnt == pkt_word)
             {
@@ -121,22 +124,43 @@ void cyt_rdma_arbiter_data(
             STREAM_WRITE(m_axis_0, currWord);
         }
         break;
-    case WRITE_STREAM:
-        if (!s_axis.empty())
+    case WRITE_STREAM_CARD:
+        if (!s_axis_0.empty())
         {
-            currWord = STREAM_READ(s_axis);
+            currWord = STREAM_READ(s_axis_0);
             ap_axiu<512, 0, 0, 8> outWord;
 
             outWord.data = currWord.data;
             outWord.keep = currWord.keep;
             outWord.last = currWord.last;
-            outWord.dest = meta_internal_stream;
+            outWord.dest = meta_internal_host; // use the host flag to indicate whether it is to host or device
             word_cnt++;
             
             if (word_cnt == pkt_word)
             {
                 word_cnt = 0;
-                currWord.last = 1;
+                outWord.last = 1;
+                fsmState = META;
+            }
+            STREAM_WRITE(m_axis_1, outWord);
+        }
+        break;
+    case WRITE_STREAM_HOST:
+        if (!s_axis_1.empty())
+        {
+            currWord = STREAM_READ(s_axis_1);
+            ap_axiu<512, 0, 0, 8> outWord;
+
+            outWord.data = currWord.data;
+            outWord.keep = currWord.keep;
+            outWord.last = currWord.last;
+            outWord.dest = meta_internal_host; // use the host flag to indicate whether it is to host or device
+            word_cnt++;
+            
+            if (word_cnt == pkt_word)
+            {
+                word_cnt = 0;
+                outWord.last = 1;
                 fsmState = META;
             }
             STREAM_WRITE(m_axis_1, outWord);
@@ -145,15 +169,18 @@ void cyt_rdma_arbiter_data(
     }
 } 
 
-// check the host bit of the s_meta, which corresponds to the wr_req
-// if host bit equals 0, this is a SEND Verb, route meta to eth notification and route data stream to channel 0
-// if host bit equals 1, this is an WRITE Verb, route meta and data to channel 1
-// if data routes to channel 1, set the meta_internal field according to the stream flag in the cyt_req_t to indicate host/card
+// check the command type of the s_meta, which corresponds to the rq_wr
+// the input data stream can come from either s_axis_0/s_axis_1, corresponding to rrsp_recv_0/1. It should be selected based on the dest field of the s_meta
+// if the dest is CYT_STRM_CARD, then consume from s_axis_0, and if dest is CYT_STRM_HOST, consume from s_axis_1
+// if strm flag is CYT_STRM_RDMA, and the opcode is CYT_RDMA_SEND, this is a SEND Verb, route meta to eth notification and route data stream to channel 0
+// if strm flag is CYT_STRM_RDMA, and the opcode is CYT_RDMA_WRITE, this is a WRITE Verb, route meta and data to channel 1
+// if data routes to channel 1 (CYT_RDMA_WRITE), set the meta_internal field according to the dest flag in the cyt_req_t to indicate host/card
 // compact bit pragma required for cyt_req_t as this interfaces with Coyote. 
 
 void cyt_rdma_arbiter(
                 hls::stream<cyt_req_t >& s_meta,
-                hls::stream<ap_axiu<512, 0, 0, 8> >& s_axis,
+                hls::stream<ap_axiu<512, 0, 0, 8> >& s_axis_0,
+                hls::stream<ap_axiu<512, 0, 0, 8> >& s_axis_1,
                 hls::stream<eth_notification >& m_meta_0,
                 hls::stream<ap_axiu<512, 0, 0, 8> >& m_axis_0,
                 hls::stream<cyt_req_t>& m_meta_1,
@@ -161,7 +188,8 @@ void cyt_rdma_arbiter(
                 )
 {
 #pragma HLS INTERFACE axis register  port=s_meta
-#pragma HLS INTERFACE axis register  port=s_axis
+#pragma HLS INTERFACE axis register  port=s_axis_1
+#pragma HLS INTERFACE axis register  port=s_axis_0
 #pragma HLS INTERFACE axis register  port=m_meta_0
 #pragma HLS INTERFACE axis register  port=m_axis_0
 #pragma HLS INTERFACE axis register  port=m_meta_1
@@ -186,7 +214,8 @@ void cyt_rdma_arbiter(
 
     cyt_rdma_arbiter_data(	
                 meta_int,
-                s_axis,
+                s_axis_0,
+                s_axis_1,
                 m_axis_0,
                 m_axis_1
     );
